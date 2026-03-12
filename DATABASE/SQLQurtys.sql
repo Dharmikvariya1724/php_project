29bb189d7e0a618a2b16bed0348cbe86b6102199
Dharmikvariya1724 <dharmikvariya273@gmail.com>
2026-03-12 19:09:03 +0530

    all sql Query

--- DATABASE/SQLQurtys.sql
+++ DATABASE/SQLQurtys.sql
@@ -8,14 +8,16 @@ SHOW DATABASES;
 USE STUDENT;
 
 /* Create Table in DATABSE  */
-CREATE TABLE users (
-id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(100),
-email VARCHAR(100) NOT NULL UNIQUE,
-password VARCHAR(255),
+CREATE TABLE admin (
+ad_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ad_name VARCHAR(100),
+ad_email VARCHAR(100) NOT NULL UNIQUE,
+ad_password VARCHAR(255),
+ad_city VARCHAR(255),
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+INSERT INTO admin (ad_name,ad_email,ad_password,ad_city) VALUES ('Aasif','aasif@gmail.com','786','surat'), ('Kirtan','dhola@123gmail.com','12345','Bhopal');
 /* READ */
 /*View All Users Query:*/ 
 SELECT * FROM `Users`;
@@ -129,4 +131,87 @@ COMMIT;
 UPDATE EMP SET name='Aasif132',AGE='28',PHONE='231465789',email='aasif123@gmail.com',password='123457' WHERE id=5;
 ROLLBACK;
 
-/*  PRIMARY KEY & FOREIGN KEY */
+/*  PRIMARY KEY */
+-- CREATE NEW TABLE AN USING PRIMARY KEY
+CREATE TABLE emp (
+id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(255),
+age INT NOT NULL CHECK(AGE>=18),
+phone VARCHAR(15) UNIQUE,
+email VARCHAR(255) NOT NULL UNIQUE,
+password VARCHAR(255),
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (ID),
+);
+-- jo table pahle thi create hoy to aa cammand no use thay che,
+ALTER TABLE EMP ADD PRIMARY KEY (id);
+
+/* FOREIGN KEY */
+-- Create a New Table using foreign key
+CREATE TABLE emp (
+id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(255),
+age INT NOT NULL CHECK(AGE>=18),
+phone VARCHAR(15) UNIQUE,
+admin_id int(15),
+email VARCHAR(255) NOT NULL UNIQUE,
+password VARCHAR(255),
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+ -- REFRENCE LEY CHE User Table Mathi U_id Mathi Name Filed
+);
+-- jo table pahle thi create hoy to aa cammand no use thay che,
+ALTER TABLE EMP ADD FOREIGN KEY (admin_id) REFERENCES admin(ad_id);
+
+ALTER TABLE EMP ADD admin_id int(15),
+
+INSERT INTO EMP (name,age,phone,admin_id,email,password) VALUES ('Aasif','28','9874561235','2','aasif@gmail.com','786'), ('Kirtan','23','9874651235','1','dhola@123gmail.com','12345');
+
+/* INNER JOIN */
+-- Two table to data Show.
+SELECT * FROM ADMIN A INNER JOIN EMP E ON A.AD_ID = E.CITY;
+-- view a Selected filad in Table
+SELECT e.id,e.name,e.age,e.phone,e.city,e.email,e.password,a.ad_city FROM ADMIN A INNER JOIN EMP E ON A.AD_ID = E.CITY WHERE ad_city= 'surat';
+-- With INNER TEXT TO USE A QUERY AND CONDITION RUN.
+SELECT e.id,e.name,e.age,e.phone,e.city,e.email,e.password,a.ad_city FROM ADMIN A JOIN EMP E ON A.AD_ID = E.CITY WHERE ad_city= 'surat';
+
+/* LEFT JOIN */
+SELECT emp.id,emp.name,emp.age,emp.phone,emp.admin_id,emp.email,emp.password,admin.ad_city FROM emp LEFT JOIN admin ON emp.admin_id = admin.ad_id; -- left join using clounm name
+
+/* RIGHT JOIN */
+SELECT * FROM ADMIN RIGHT JOIN EMP ON ADMIN.AD_ID = EMP.ADMIN_ID;-- Right join Using With out colunm name
+
+/* CROSS JOIN */
+SELECT * FROM EMP CROSS JOIN ADMIN;
+
+/* Multiple JOIN */
+
+-- New Table Create
+CREATE TABLE work (
+id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+w_name VARCHAR(255),
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Multiple data Insert.
+INSERT INTO WORK (W_NAME) VALUES('excel'),('data listing'),('devlopment'),('devoops');
+ALTER TABLE EMP ADD FOREIGN KEY (work_id) REFERENCES work(w_id);
+/* Multiple JOIN */
+-- TWO OR MORE THAN TABLE TO DATA FACHINMG USE
+SELECT * FROM EMP INNER JOIN ADMIN ON EMP.ADMIN_ID = ADMIN.AD_ID INNER JOIN WORK ON EMP.WORK_ID = WORK.W_ID;
+
+/* Group By */
+SELECT work_id, COUNT(work_id) FROM emp GROUP BY work_id;
+
+/* HAVING Clause */
+
+SELECT admin_id, COUNT(*) AS total_employee FROM emp GROUP BY admin_id HAVING COUNT(*) > 2;
+
+/* SubQuery */
+-- FIND DATA USING NAME FILED
+SELECT name FROM emp WHERE work_id = ( SELECT w_id FROM work WHERE w_name = "devoops");
+
+/* UNION */
+-- Same datatype ane same colum hovi joy ye abbe same order ma. 
+SELECT EMP.ID , EMP.NAME FROM EMP UNION SELECT WORK.W_ID,WORK.W_NAME FROM WORK;
+/* UNION ALL */
+SELECT EMP.ID , EMP.NAME FROM EMP UNION ALL SELECT WORK.W_ID,WORK.W_NAME FROM WORK;
